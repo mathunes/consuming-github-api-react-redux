b27675b7d44e3b74d817768203f4bfea2c335691
Matheus Antunes Vieira <matheusantunes@id.uff.br>
2020-01-25 10:27:58 -0300

    Updates

---
+++ precache-manifest.727353dcc93ddac7958fdba0c0077c92.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5af4502fd5d36ddb9471385f9ee67d13",
+    "url": "/consuming-github-api-react-redux/index.html"
+  },
+  {
+    "revision": "8f4086d6fd899cae3113",
+    "url": "/consuming-github-api-react-redux/static/css/main.24074f60.chunk.css"
+  },
+  {
+    "revision": "8445b0336f3ad2d9efe7",
+    "url": "/consuming-github-api-react-redux/static/js/2.e746512e.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/consuming-github-api-react-redux/static/js/2.e746512e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8f4086d6fd899cae3113",
+    "url": "/consuming-github-api-react-redux/static/js/main.d7a511fd.chunk.js"
+  },
+  {
+    "revision": "695e3aebe83f1ebc5bd0",
+    "url": "/consuming-github-api-react-redux/static/js/runtime-main.96f08584.js"
+  },
+  {
+    "revision": "f7b319ad7fc4e77e7745210539f68a71",
+    "url": "/consuming-github-api-react-redux/static/media/github.f7b319ad.svg"
+  },
+  {
+    "revision": "4c29a07a5d6e57b6abf585c6a6c334c6",
+    "url": "/consuming-github-api-react-redux/static/media/loading.4c29a07a.gif"
+  },
+  {
+    "revision": "bf212733d3ec4acc1c610d628bf8ab97",
+    "url": "/consuming-github-api-react-redux/static/media/search.bf212733.svg"
+  }
+]);

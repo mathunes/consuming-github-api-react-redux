470dad78ca43d27b9bcef479f236f205b9c0aca8
Matheus Antunes Vieira <matheusantunes@id.uff.br>
2020-01-24 23:04:01 -0300

    Estilizacao no titulo e na logo

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -3,6 +3,7 @@ import { bindActionCreators } from 'redux';
 import * as searchActions from '../../actions/search.js';
 import { connect } from 'react-redux';
 import './style.css';
+import logoGithub from '../../assets/images/github.svg';
 
 class Header extends Component {
     state = {
@@ -18,6 +19,10 @@ class Header extends Component {
     render() {
         return (
             <header>
+                <div className="title-container">
+                    <img src={logoGithub} alt="Logo github" className="logo"/>
+                    <h1 className="title-header">GitHub Usuários</h1>
+                </div>
                 <form onSubmit={this.searchUser}>
                     <input type="text" placeholder="Pesquisar..." onChange={(e) => {this.setState({user: e.target.value})}}/>
                     <input type="submit" value="Buscar"/>
